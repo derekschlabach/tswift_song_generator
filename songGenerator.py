6acc4ae8343f39336721e5b45bfce8873e959f06
derekschlabach <derekschlabach@gmail.com>
2015-12-10 14:36:50 -0600

    staged

--- songGenerator.py
+++ songGenerator.py
@@ -7,31 +7,61 @@ import re
 import random
 
 file_name = "corpus/tswift-full.txt"
-raw_words = re.findall(r'[a-z\'0-9]+', file(file_name).read().lower())
-nltk_text = nltk.Text(raw_words)
+raw_lines = filter(None, [ re.sub(r'\([^)]*\)|^[a-z\'0-9]', '', line).strip().lower() for line in open(file_name).readlines()])
+raw_words = re.findall(r'[a-z\'0-9]+', ' '.join(raw_lines))
 vocab = set(raw_words)
 
 lm_data = LanguageModelData(raw_words)
 langmod = TrigramLanguageModel(lm_data)
 
+#Taken from Proj 4
+penntb_to_reduced = {}
+# noun-like
+for x in ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'EX', 'WP'] :
+    penntb_to_reduced[x] = 'N'
+# verb-like
+for x in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'MD', 'TO'] :
+    penntb_to_reduced[x] = 'V'
+# adjective-like
+for x in ['POS', 'PRP$', 'WP$', 'JJ', 'JJR', 'JJS', 'DT', 'CD', 'PDT', 'WDT', 'LS']:
+    penntb_to_reduced[x] = 'AJ'
+# adverb-like
+for x in ['RB', 'RBR', 'RBS', 'WRB', 'RP', 'IN', 'CC']:
+    penntb_to_reduced[x] = 'AV'
+# interjections
+for x in ['FW', 'UH'] :
+    penntb_to_reduced[x] = 'I'
+# symbols
+for x in ['SYM', '$', '#'] :
+    penntb_to_reduced[x] = 'S'
+# groupings
+for x in ['\'\'', '(', ')', ',', ':', '``'] :
+    penntb_to_reduced[x] = 'G'
+# end-of-sentence symbols
+penntb_to_reduced['.'] = 'E'
+penntb_to_reduced['-NONE-'] = '-NONE-'
+
+reduced_tags = ['N', 'V', 'AJ', 'AV', 'I', 'S', 'G', 'E']
+
+
 #use nltk tags not reduced
 #make map of pos to sets of words of that pos from nltk's tags
 words_in_pos = {}
-#all_tagged = nltk.pos_tag(nltk_text)
 all_tagged_vocab = nltk.pos_tag(nltk.Text(vocab))
+all_tagged_vocab = [ (word, penntb_to_reduced[tag]) for (word, tag) in all_tagged_vocab ]
 for (word, tag) in all_tagged_vocab :
     if tag in words_in_pos :
         words_in_pos[tag].append(word)
     else :
         words_in_pos[tag] = [word]
 
-#print words_in_pos
 for pos in words_in_pos :
     print pos
     print words_in_pos[pos]
 
     print
     print
+
 
  
 def get_word_for_pos(pos, hist):
