b7a772cd0f5339aaf4ca44064883f99e084785aa
Kyle Kent <kyle.kent@my.wheaton.edu>
2015-12-10 22:53:16 -0600

    Added better line generation techniques

--- songGenerator.py
+++ songGenerator.py
@@ -23,21 +23,35 @@ words_in_pos = {}
 for tag in reduced_tagset :
    words_in_pos[tag] = []
 
-line_structures = []   
-
+line_structures = []
+#this should be a map of every possible line ending to every possible line beginning that can follow
+valid_line_transitions = {}   
+previous = '0'
 for line in raw_lines:
     tagged_line = nltk.pos_tag(nltk.Text(re.findall(r'[a-z\'0-9]+', line)))
     currentStruct = [ reduced_tgs[tag] for (word, tag) in tagged_line ]
     
     for (word, tag) in tagged_line :
+        #print word, tag
+        indexTag = tag #need this because reduced tags are not in tagged line
         tag = reduced_tgs[tag]
         if word not in words_in_pos[tag] :
-            words_in_pos[tag].append(word)      
+            words_in_pos[tag].append(word)
+    
+        if tagged_line.index((word, indexTag)) == len(tagged_line)-1:
+            if previous == 0 or previous not in valid_line_transitions:
+                valid_line_transitions[previous] = set()
+            
+            else:
+                valid_line_transitions[previous].add(tag)
+            previous = tag
 
     if line_structures is None or currentStruct not in line_structures:
         line_structures.append(currentStruct)
         
 
+print "valid transitions:" + str(valid_line_transitions) + '\n'
+        
 #print "line_structures:" + str(line_structures[:10]) + '\n'
 #print "Number of line_structures:" + str(len(line_structures))
 
@@ -62,13 +76,13 @@ def get_word_for_pos(pos, hist):
     
     if sumProbs == 0.0:
         for word in words_in_pos[pos]:
-            prob = langmod.p(word, [''])
+            prob = langmod.p(word, [])
             word_probs[word] = prob
             sumProbs += word_probs[word]
     
     #choose random number from 0.00000001 to sumProbs (inclusive)
     rand = random.random() * sumProbs
-    print str(sumProbs) + ": " + str(rand)
+    #print str(sumProbs) + ": " + str(rand)
     counter = 0.0
     for word in word_probs:
         counter += word_probs[word]
@@ -77,18 +91,30 @@ def get_word_for_pos(pos, hist):
             return word
         
     #if we do not return anything we should throw an exception
+prev_ending = '0'
+for x in range(0, 10):
+    if prev_ending != '0':
+        while True:
+            struct = line_structures[int(random.random() * len(line_structures))]
+            #print struct[-1]
+            if struct[-1] in valid_line_transitions[prev_ending]:
+                break
     
-struct = line_structures[int(random.random() * len(line_structures))]
-line_to_build = []
-
-for pos in struct:
-    line_to_build.append(get_word_for_pos(pos, line_to_build))
-for i in range(10) :
-    print get_word_for_pos('NS', [])
-
-print "Length of line_to_build:" + str(len(line_to_build))
-print struct
-print line_to_build
+    else:
+        struct = line_structures[int(random.random() * len(line_structures))]
+        prev_ending = struct[-1]
+        
+    line_to_build = []
+    
+    for pos in struct:
+        line_to_build.append(get_word_for_pos(pos, line_to_build))
+    
+    #for i in range(10) :
+    #   print get_word_for_pos('NS', [])
+    
+    print "Length of line_to_build:" + str(len(line_to_build))
+    print struct
+    print line_to_build
 
 
 
